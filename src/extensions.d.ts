13a8e5d16b669550a400da62d8ddc2f13217ab7e
zinhumgato <zinhumgato@gmail.com>
2019-04-09 12:49:24 +0700

    fix error

--- src/extensions.d.ts
+++ src/extensions.d.ts
@@ -1,5 +1,37 @@
 declare interface Array<T> {
-    firstOrDefault(callback?: any): T;
-    clone(): T;
+    firstOrDefault(predicate?: any): T;
+}
 
+declare interface Object {
+    clone(): any;
+}
+
+declare interface String {
+    format(...parameters: string[]): string;
+}
+
+declare interface StringConstructor {
+    format(...parameters: string[]): string;
+}
+
+
+declare interface Window {
+    ioc: IIoCContainer;
+}
+
+declare interface IIoCContainer {
+    import(registrations: Array<IIoCRegistration>): void;
+    resolve(nameOrType: any): any;
+}
+
+declare interface IIoCRegistration {
+    name: string;
+    instanceOf: any;
+    lifeCycle: IIoCLifeCycle,
+    instance?: any
+}
+
+declare enum IIoCLifeCycle {
+    Singleton = 1,
+    Transient = 2
 }
