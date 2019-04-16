85fb27bcea2078973588c9185dea41598f8c33ec
zinto <zinhumgato@gmail.com>
2019-04-17 03:09:37 +0700

    fix error

--- src/_shared/services/resourceManager.ts
+++ src/_shared/services/resourceManager.ts
@@ -3,21 +3,24 @@ import { IResourceManager } from "./iresourceManager";
 import { Promise, PromiseFactory } from "../../models/promise";
 import { IConnector } from "../connector/iConnector";
 export class ResourceManager implements IResourceManager {
-    public locales: any = {};
-    public languageCode: LanguageCodes = LanguageCodes.EN;
-    public getLocales(): Array<any> {
+    private locales: any = {};
+    private localeNames: Array<string> = [];
+    private languageCode: string = LanguageCodes.EN;
+    public getLocales(): any {
         return this.locales;
     }
 
-    public changeLanguageCode(languageCode: LanguageCodes): void {
+    public changeLanguageCode(languageCode: string): void {
         this.languageCode = languageCode;
+        this.loadLocales(this.localeNames);
     }
 
-    public loadLocales(localeNames: Array<any>): Promise {
-        localeNames = localeNames || [];
+    public loadLocales(locales: Array<any>): Promise {
+        this.localeNames = locales;
+        locales = locales || [];
         let promise = PromiseFactory.create();
         let self = this;
-        localeNames.forEach((name: string) => {
+        locales.forEach(name => {
             self.getLocaleByName(name).then(() => {
                 promise.resolve();
             });
@@ -26,14 +29,14 @@ export class ResourceManager implements IResourceManager {
     }
 
     private getLocaleByName(name: string): Promise {
-        let self = this;
-        let url = String.format("/resources/locales/{0}.{1}.json", name, this.languageCode);
         let promise = PromiseFactory.create();
-        let iConnector: IConnector = window.ioc.resolve(IoCNames.IConnector);
-        iConnector.get(url).then((jsonData: any) => {
-            self.locales[name] = jsonData;
+        let iconnector: IConnector = window.ioc.resolve(IoCNames.IConnector);
+        let url = String.format("/resources/locales/{0}.{1}.json", name, this.languageCode);
+        iconnector.get(url).then((jsonData: any) => {
+            this.locales[name] = jsonData;
             promise.resolve();
         });
         return promise;
     }
+
 }
